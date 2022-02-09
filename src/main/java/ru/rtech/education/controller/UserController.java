3f3793cb5737c4a067e16d19c8fe6e2583af7cf3
yusmankov <yusmankov@gmail.com>
2022-02-09 21:04:55 +0500

    add mini logic

--- src/main/java/ru/rtech/education/controller/UserController.java
+++ src/main/java/ru/rtech/education/controller/UserController.java
@@ -29,7 +29,7 @@ public class UserController {
     }
 
     @DeleteMapping("/users/{id}")
-    public void deleteUser(@PathVariable("id")  Long id){
+    public void deleteUser(@PathVariable("id") Long id){
          userService.deleteUser(id);
     }
 }
