22c9e05e7dc0557df291e05ec0d4e92211796733
yusmankov <yusmankov@gmail.com>
2022-02-09 18:46:26 +0500

    new project

---
+++ src/main/java/ru/rtech/education/controller/UserController.java
@@ -0,0 +1,15 @@
+package ru.rtech.education.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import ru.rtech.education.model.User;
+
+@RestController
+public class UserController {
+
+    @PostMapping(value = "/users")
+    public User createUser(@RequestBody User user){
+        return new User(1L,"login","Ivan","Ivanov");
+    }
+}
---
+++ src/main/java/ru/rtech/education/model/User.java
@@ -0,0 +1,17 @@
+package ru.rtech.education.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private long id;
+    private String login;
+    private String firstName;
+    private String lastName;
+}
